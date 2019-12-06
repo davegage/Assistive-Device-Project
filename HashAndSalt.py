621b51c663bc6f683f97b720de5e94cf77f7c761
davegage <davedavedave96@gmail.com>
2019-12-06 15:35:09 -0500

    Add files via upload

---
+++ HashAndSalt.py
@@ -0,0 +1,7 @@
+import bcrypt
+
+def get_hashed_password(plain_text_password):
+    return bcrypt.hashpw(plain_text_password, bcrypt.gensalt())
+	
+def check_password(plain_text_password, hashed_password):
+    return bcrypt.checkpw(plain_text_password, hashed_password)
