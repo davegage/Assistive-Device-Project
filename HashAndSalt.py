f645f014dd81fef796a7483e422bdb737cb3f565
davegage <davedavedave96@gmail.com>
2019-12-08 13:54:55 -0500

    Add files via upload

--- HashAndSalt.py
+++ HashAndSalt.py
@@ -3,5 +3,12 @@ import bcrypt
 def get_hashed_password(plain_text_password):
     return bcrypt.hashpw(plain_text_password, bcrypt.gensalt())
 	
-def check_password(plain_text_password, hashed_password):
-    return bcrypt.checkpw(plain_text_password, hashed_password)
+def check_password(plain_text_password):
+	with open('C:\Test Files\password.pwd') as f:
+		first_line = f.readline()
+		return bcrypt.checkpw(plain_text_password, first_line)
+	
+def save_password(plain_text_password):
+	f = open("C:\Test Files\testpassword.pwd", "w")
+	f.write(bcrypt.hashpw(plain_text_password, bcrypt.gensalt()))
+	f.close()
